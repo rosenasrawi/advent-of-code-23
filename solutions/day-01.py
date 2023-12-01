ab87cf233af2ad3373ec5518c48e2291e58bae29
Rose Nasrawi <r.nasrawi11@gmail.com>
2023-12-01 11:19:23 +0100

    Add to calib_sum and refactor

--- solutions/day-01.py
+++ solutions/day-01.py
@@ -3,27 +3,36 @@ import regex as re
 
 calibration = getinput(day = '01')
 
-def sum_nums(calibration, inclwords = False):
+def get_sum(calibration, word_incl = False):
 
-    word2num = {'one': 1, 'two': 2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
-    pattern = '|'.join(word2num.keys())
+    word2num = {
+        'one': '1', 
+        'two': '2', 
+        'three': '3', 
+        'four': '4', 
+        'five': '5', 
+        'six': '6', 
+        'seven': '7', 
+        'eight': '8', 
+        'nine': '9'
+    }
 
-    calib_int = []
+    words = '|'.join(word2num.keys())
+
+    calib_sum = 0
 
     for c in calibration:
 
-        if inclwords:
-            numbers = re.findall(rf'{pattern}|\d', c, overlapped=True)
-            numbers = [str(word2num[num]) if num in word2num.keys() else num for num in numbers]
+        if word_incl:
+            nums = re.findall(rf'{words}|\d', c, overlapped=True)
+            nums = [word2num[n] if n in word2num.keys() else n for n in nums]
 
         else: 
-            numbers = re.findall(r'\d', c)
+            nums = re.findall(r'\d', c)
 
-        numbers = int(numbers[0]+numbers[-1])
+        calib_sum += int(nums[0]+nums[-1])
 
-        calib_int.append(numbers)
+    return calib_sum
 
-    return sum(calib_int)
-
-print('Part 1:', sum_nums(calibration))
-print('Part 2:', sum_nums(calibration, inclwords=True))
+print('Part 1:', get_sum(calibration))
+print('Part 2:', get_sum(calibration, word_incl=True))
