1bc57ca5f7668ef39e0f804575e0a24bee42523e
Rose Nasrawi <r.nasrawi11@gmail.com>
2023-12-11 23:05:17 +0100

    Solve day 11

--- solutions/day-11.py
+++ solutions/day-11.py
@@ -2,34 +2,36 @@ from _getinput import *
 
 # --- Day 11: Cosmic Expansion ---
 
-def insert_empty(input):
+def update_galaxies(input, galaxies, factor, flip = False):
     
-    skip = False
-
+    if flip: 
+        input = list(map(list, zip(*input)))
+        pos = 1
+    else: pos = 0
+    
+    count = 0
     for r, line in enumerate(input):
-        if skip:
-            skip=False
-            continue
         if '#' not in line:
-            input.insert(r,line)
-            skip = True
+            for i in range(len(galaxies)):
+                if galaxies[i][pos] > r+count:
+                    galaxies[i][pos] += factor-1
+            count += factor-1
+    
+    return galaxies
 
-    return input
-
-def cosmic_expansion():
+def cosmic_expansion(factor = 2):
 
     input = getinput(day='11', example=False)
 
-    for _ in range(2):
-        input = insert_empty(input)
-        input = list(map(list, zip(*input)))
-    
     galaxies = []
 
     for r, line in enumerate(input):
         for c, g in enumerate(line):
-            if g == '#': galaxies.append((r,c))
+            if g == '#': galaxies.append([r,c])
 
+    galaxies = update_galaxies(input, galaxies, factor)
+    galaxies = update_galaxies(input, galaxies, factor, flip = True)
+    
     return galaxies
 
 def find_path(g1, g2):
@@ -37,28 +39,13 @@ def find_path(g1, g2):
     r1, c1 = g1
     r2, c2 = g2
 
-    rd = 1 if r2-r1 > 0 else -1
-    cd = 1 if c2-c1 > 0 else -1
+    steps = abs(r1-r2) + abs(c1-c2)
 
-    steps = 0
-
-    while True:
-        if r1 == r2: rd = 0
-        if c1 == c2: cd = 0
-
-        if cd == 0 and rd == 0:
-            break
-
-        r1+=rd; c1+=cd
-        if rd == 0 or cd == 0:
-            steps += 1
-        else: steps += 2
-    
     return steps
 
-def connect_galaxy():
+def connect_galaxy(factor):
 
-    galaxies = cosmic_expansion()
+    galaxies = cosmic_expansion(factor)
     total = 0
 
     while galaxies:
@@ -69,8 +56,5 @@ def connect_galaxy():
         
     return total
 
-print('Part 1:', connect_galaxy())
-
-
-
-    
+print('Part 1:', connect_galaxy(factor = 2))
+print('Part 2:', connect_galaxy(factor = 1000000))
