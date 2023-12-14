8356dbb843cbadf8da87011b28cda1ecf6aa036c
Rose Nasrawi <r.nasrawi11@gmail.com>
2023-12-14 16:01:22 +0100

    Solve day 14

--- solutions/day-14.py
+++ solutions/day-14.py
@@ -1,5 +1,4 @@
 from _getinput import *
-import regex as re
 
 # --- Day 14: Parabolic Reflector Dish ---
 
@@ -37,10 +36,51 @@ def get_load(input):
     
     return total
 
-input = getinput(day='14', example=False)
-input = [list(line) for line in input]
+def turn_platform(input):
+    return list(map(list,list(zip(*input[::-1]))))
 
-stones = find_stones(input)
-input = tilt_lever(stones,input)
+def total_load():
 
-print('Part 1:', get_load(input))
+    input = getinput(day='14', example=False)
+    input = [list(line) for line in input]
+
+    stones = find_stones(input)
+    input = tilt_lever(stones,input)
+
+    return get_load(input)
+
+def find_cycle():
+
+    input = getinput(day='14', example=False)
+    input = [list(line) for line in input]
+
+    tup_input = tuple(''.join(line) for line in input)
+    states = {}; step = 1
+
+    while True:
+
+        for _ in range(4):
+            stones = find_stones(input)
+            input = tilt_lever(stones,input)
+            input = turn_platform(input)
+
+        tup_input = tuple(''.join(line) for line in input)
+        
+        if tup_input in states:
+            first_occ = list(states.keys()).index(tup_input)
+            cycle_len = len(states)-first_occ
+            left = (1000000000 - len(states)-1) % cycle_len
+
+            for _ in range(left):
+                for _ in range(4):
+                    stones = find_stones(input)
+                    input = tilt_lever(stones,input)
+                    input = turn_platform(input)
+
+            return get_load(input)
+
+        states[tup_input] = step
+        step+=1
+
+print('Part 1:', total_load())
+print('Part 2:', find_cycle())
