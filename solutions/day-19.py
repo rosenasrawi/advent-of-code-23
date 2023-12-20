bf72b3bb56492eefa6a029c9d07e0550571ef0a5
Rose Nasrawi <r.nasrawi11@gmail.com>
2023-12-20 16:45:44 +0100

    Solve day 19

--- solutions/day-19.py
+++ solutions/day-19.py
@@ -58,4 +58,57 @@ def move_parts(accept = 0):
 
     return accept
 
+def get_combo(ranges, accept=1):
+
+    for let in 'xmas':
+        low, high = ranges[let]
+        accept *= high-low+1
+    
+    return accept
+
+def eval_range(item, ranges, rules):
+
+    if item == 'A':
+        return get_combo(ranges)
+    if item == 'R':
+        return 0
+    
+    accept = 0
+    rule = rules[item]
+
+    for rl in rule:
+
+        if len(rl) == 2:
+            r, item = rl
+            low, high = ranges[r[0]]
+            rng_yes = ranges.copy()
+
+            if '<' in r:
+                let, num = r.split('<')
+                
+                rng_yes[let] = [low, int(num)-1]
+                ranges[let] = [int(num), high]
+
+            elif '>' in r:
+                let, num = r.split('>')
+                
+                ranges[let] = [low, int(num)]
+                rng_yes[let] = [int(num)+1, high]
+
+            accept += eval_range(item, rng_yes, rules)
+        
+        else:
+            item = rl[0]
+            accept += eval_range(item, ranges, rules)
+
+    return accept
+
+def move_ranges():
+
+    _, rules = parse_hell()
+    item, ranges = 'in', {let:[1,4000] for let in 'xmas'}
+
+    return eval_range(item, ranges, rules)
+
 print('Part 1:', move_parts())
+print('Part 2:', move_ranges())
