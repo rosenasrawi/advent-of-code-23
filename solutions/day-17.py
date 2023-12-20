fd9cffe773e67d9a8a715d54bb650663e9734d03
Rose Nasrawi <r.nasrawi11@gmail.com>
2023-12-20 11:23:40 +0100

    Solve day 17 yay

--- solutions/day-17.py
+++ solutions/day-17.py
@@ -3,13 +3,15 @@ import heapq, time
 
 # --- Day 17: Clumsy Crucible ---
 
-def find_path():
+def find_path(ultra = False):
 
     t1 = time.time()
     input = getinput(day='17', example=False)
-    end = (len(input[0])-1, len(input)-1)
+    end = (len(input)-1, len(input[0])-1)
 
-    score, start, ploc, pdir = 0, (0, 0, None), (None, None), 'NNN'
+    score, start, ploc = 0, (0, 0, None), (None, None)
+    pdir = 'N'*3 if not ultra else 'N'*10
+
     queue = [(score, start, ploc, pdir)]
     memory = set()
 
@@ -25,15 +27,20 @@ def find_path():
 
         memory.add((current, pdir))
 
-        around = [(r, c - 1, 'l'), (r - 1, c, 'u'), (r, c + 1, 'r'), (r + 1, c, 'd')]
+        around = [(r,c-1,'l'), (r-1,c,'u'), (r,c+1,'r'), (r+1,c,'d')]
 
-        around = [a for a in around if 0 <= a[0] <= end[0] and 0 <= a[1] <= end[1]]
-        around = [a for a in around if (a[0], a[1]) != ploc]
+        around = [a for a in around if 0<=a[0]<=end[0] and 0<=a[1]<=end[1]]
+        around = [a for a in around if (a[0],a[1]) != ploc]
 
         if len(set(pdir)) == 1:
             around = [a for a in around if a[-1] != dir]
 
-        ploc = (r, c)
+        if ultra:
+            last = pdir[6:]
+            if len(set(last)) != 1 and last[-1]!='N':
+                around = [a for a in around if a[-1] == dir]
+
+        ploc = (r,c)
 
         for next in around:
             rn, cn, dn = next
@@ -45,3 +52,4 @@ def find_path():
             heapq.heappush(queue, (nscore, next, ploc, pdir[1:]+dn))
 
 print('Part 1:', find_path())
+print('Part 2:', find_path(ultra = True))
